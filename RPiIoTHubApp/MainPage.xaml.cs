7bd47c45f263b2496eaa133e1cbfc361e4b9c2f7
Akira Inoue <chack.inoue@hotmail.com>
2016-05-27 11:49:46 +0900

    プロジェクト ファイルを追加します。

---
+++ RPiIoTHubApp/MainPage.xaml.cs
@@ -0,0 +1,278 @@
+﻿//#define ACCESS_MOBILE_SERVICE
+#define ACCESS_IOT_HUB
+using IoTDevice;
+using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.Web.Http;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace RPiIoTHubApp
+{
+#if (ACCESS_MOBILE_SERVICE)
+    using Microsoft.WindowsAzure.MobileServices;
+#endif
+#if (ACCESS_IOT_HUB)
+    using Microsoft.Azure.Devices.Client;
+    using Newtonsoft.Json;
+    using System.Net;
+#endif
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // Device Entry Configuration
+        string DeviceEntryEndPoint = "http://[MobileAppName].azurewebsites.net";
+
+        // Identifier of this board. this value will be set by this app.
+        string deviceId = "";
+        double Latitude = 35.62661;
+        double Longitude = 139.740987;
+
+        // IoT Hub Configuration
+        string IoTHubEndpoint = "JPHack.azure-devices.net";
+        string DeviceKey = "zPmuVka/fdpCMeryWyqnDCpVijtPgu8OZpYm7M4f9a0=";
+        bool IoTServiceAvailabled = true;
+
+        private DispatcherTimer measureTimer;
+        private long measureIntervalMSec = 2000;
+
+        private DispatcherTimer uploadTimer;
+        private long uploadIntervalMSec = 10000;
+
+        IoTKitHoLSensor sensor;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.Loaded += MainPage_Loaded;
+        }
+
+        private async void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {
+            FixDeviceId();
+            var result = await TryConnect();
+            sensor = IoTKitHoLSensor.GetCurrent(IoTKitHoLSensor.TemperatureSensor.BME280);
+            measureTimer = new DispatcherTimer();
+            measureTimer.Interval = TimeSpan.FromMilliseconds(measureIntervalMSec);
+            measureTimer.Tick += MeasureTimer_Tick;
+            measureTimer.Start();
+
+            InitializeUpload();
+        }
+
+        async Task<bool> TryConnect()
+        {
+            bool result = false;
+            var client = new Windows.Web.Http.HttpClient();
+            client.DefaultRequestHeaders.Add("device-id", deviceId.ToString());
+            client.DefaultRequestHeaders.Add("device-message", "Hello from RPi2");
+            var response = client.GetAsync(new Uri("http://egholservice.azurewebsites.net/api/DeviceConnect"), HttpCompletionOption.ResponseContentRead);
+            response.AsTask().Wait();
+            var responseResult = response.GetResults();
+            if (responseResult.StatusCode == Windows.Web.Http.HttpStatusCode.Ok)
+            {
+                result = true;
+                var received = await responseResult.Content.ReadAsStringAsync();
+                Debug.WriteLine("Recieved - " + received);
+            }
+            else
+            {
+                Debug.WriteLine("TryConnect Failed - " + responseResult.StatusCode);
+            }
+            return result;
+        }
+
+        private void FixDeviceId()
+        {
+            foreach (var hn in Windows.Networking.Connectivity.NetworkInformation.GetHostNames())
+            {
+                IPAddress ipAddr;
+                if (!hn.DisplayName.EndsWith(".local") && !IPAddress.TryParse(hn.DisplayName, out ipAddr))
+                {
+                    deviceId = hn.DisplayName;
+                    break;
+                }
+            }
+        }
+
+        void InitializeUpload()
+        {
+            EntryDevice();
+            if (IoTServiceAvailabled)
+            {
+                SetupIoTHub();
+                uploadTimer = new DispatcherTimer();
+                uploadTimer.Interval = TimeSpan.FromMilliseconds(uploadIntervalMSec);
+                uploadTimer.Tick += UploadTimer_Tick;
+                uploadTimer.Start();
+            }
+        }
+
+        private void MeasureTimer_Tick(object sender, object e)
+        {
+            var sensorReading = sensor.TakeMeasurement();
+            lock (this)
+            {
+                lastTemperature = sensorReading.Temperature;
+                lastHumidity = sensorReading.Humidity;
+                lastPressure = sensorReading.Pressure;
+                //lastAccelX = sensorReading.AccelX;
+                //lastAccelY = sensorReading.AccelY;
+                //lastAccelZ = sensorReading.AccelZ;
+            }
+            Debug.WriteLine("Measured:" + " T=" + sensorReading.Temperature+", H=" + sensorReading.Humidity + ", P=" + sensorReading.Pressure);
+            //Debug.WriteLine("Measured - accelx=" + sensorReading.AccelX + ",accely=" + sensorReading.AccelY + ",accelz=" + sensorReading.AccelZ + ",T=" + sensorReading.Temperature + ",H=" + sensorReading.Humidity + ",P=" + sensorReading.Pressure);
+        }
+
+        private void UploadTimer_Tick(object sender, object e)
+        {
+            uploadTimer.Stop();
+            Upload();
+            uploadTimer.Start();
+        }
+
+        int counter = 0;
+        async void Upload()
+        {
+#if (ACCESS_IOT_HUB)
+            var now = DateTime.Now;
+            var sensorReading = new Models.SensorReading()
+            {
+                msgId = deviceId.ToString() + now.ToString("yyyyMMddhhmmssfff")
+            };
+            lock (this)
+            {
+                sensorReading.deviceId = deviceId.ToString();
+                sensorReading.Temperature = lastTemperature;
+                sensorReading.Humidity = lastHumidity;
+                sensorReading.Pressure = lastPressure;
+                //sensorReading.accelx = lastAccelX;
+                //sensorReading.accely = lastAccelY;
+                //sensorReading.accelz = lastAccelZ;
+                sensorReading.time = now;
+                //sensorReading.Latitude = Latitude;
+                //sensorReading.Longitude = Longitude;
+            }
+            var payload = JsonConvert.SerializeObject(sensorReading);
+            var message = new Message(System.Text.UTF8Encoding.UTF8.GetBytes(payload));
+            try
+            {
+                await deviceClient.SendEventAsync(message);
+                Debug.WriteLine("Send[" + counter++ + "]@" + now.Ticks);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Event Hub Send Failed:" + ex.Message);
+            }
+#endif
+        }
+
+
+#if (ACCESS_MOBILE_SERVICE)
+        MobileServiceClient mobileService;
+#endif
+        private async void EntryDevice()
+        {
+#if (ACCESS_MOBILE_SERVICE)
+            if (mobileService == null)
+            {
+                mobileService = new MobileServiceClient(DeviceEntryEndPoint);
+            }
+            var table = mobileService.GetTable<Models.DeviceEntry>();
+            var registered = await table.Where((de) => de.DeviceId == deviceId.ToString()).ToListAsync();
+
+            bool registed = false;
+            if (registered != null && registered.Count > 0)
+            {
+                foreach (var re in registered)
+                {
+                    if (re.ServiceAvailable)
+                    {
+                        IoTHubEndpoint = re.IoTHubEndpoint;
+                        DeviceKey = re.DeviceKey;
+                        Debug.WriteLine("IoT Hub Service Avaliabled");
+                    }
+                    registed = true;
+                    break;
+                }
+            }
+            if (!registed)
+            {
+                var entry = new Models.DeviceEntry()
+                {
+                    DeviceId = deviceId.ToString(),
+                    ServiceAvailable = false,
+                    IoTHubEndpoint = IoTHubEndpoint,
+                    DeviceKey = DeviceKey
+                };
+                await table.InsertAsync(entry);
+            }
+#else
+            IoTServiceAvailabled = true;
+#endif
+        }
+#if (ACCESS_IOT_HUB)
+        DeviceClient deviceClient;
+        string iotHubConnectionString = "";
+#endif
+        private void SetupIoTHub()
+        {
+#if (ACCESS_IOT_HUB)
+            iotHubConnectionString = "HostName=" + IoTHubEndpoint + ";DeviceId=" + deviceId + ";SharedAccessKey=" + DeviceKey;
+            try
+            {
+                deviceClient = DeviceClient.CreateFromConnectionString(iotHubConnectionString, Microsoft.Azure.Devices.Client.TransportType.Http1);
+                Debug.Write("IoT Hub Connected.");
+                ReceiveCommands();
+            }
+            catch (Exception ex)
+            {
+                Debug.Write(ex.Message);
+            }
+#endif
+        }
+#if (ACCESS_IOT_HUB)
+        async Task ReceiveCommands()
+        {
+            Debug.WriteLine("\nDevice waiting for commands from IoTHub...\n");
+            Message receivedMessage;
+            string messageData;
+
+            while (true)
+            {
+                try
+                {
+                    receivedMessage = await deviceClient.ReceiveAsync();
+
+                    if (receivedMessage != null)
+                    {
+                        messageData = Encoding.ASCII.GetString(receivedMessage.GetBytes());
+                        Debug.WriteLine("\t{0}> Received message: {1}", DateTime.Now.ToLocalTime(), messageData);
+                        await deviceClient.CompleteAsync(receivedMessage);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("IoT Hub Receive Failed.");
+                    Debug.WriteLine(ex.Message);
+                }
+                await Task.Delay(TimeSpan.FromSeconds(10));
+            }
+
+        }
+#endif
+
+        double lastHumidity;
+        double lastPressure;
+        double lastTemperature;
+        double lastAccelX;
+        double lastAccelY;
+        double lastAccelZ;
+    }
+}
---
+++ RPiIoTHubApp/Models/SensorReading.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPiIoTHubApp.Models
+{
+    public class SensorReading
+    {
+        /// <summary>
+        /// Device Id
+        /// </summary>
+        public string deviceId { get; set; }
+        public string msgId { get; set; }
+        public DateTime time { get; set; }
+
+        /// <summary>
+        /// Temperature
+        /// </summary>
+        public double Temperature { get; set; }
+        ///// <summary>
+        ///// Acceleration X
+        ///// </summary>
+        //public double accelx { get; set; }
+        ///// <summary>
+        ///// Acceleration Y
+        ///// </summary>
+        //public double accely { get; set; }
+        ///// <summary>
+        ///// Acceleration Z
+        ///// </summary>
+        //public double accelz { get; set; }
+        /// <summary>
+        /// Measured Time
+        /// </summary>
+
+        //public double Latitude { get; set; }
+        //public double Longitude { get; set; }
+        public double Humidity { get; set; }
+        public double Pressure { get; set; }
+    }
+}
